58f3cc448591afe62776732721d086a23dcadf64
Jan Blechta <blechta@karlin.mff.cuni.cz>
2015-02-12 00:14:37 +0100

    Make elasticity code more modular.

--- elasticity/elast.py
+++ elasticity/elast.py
@@ -18,135 +18,145 @@
 # Begin code
 from dolfin import *
 
-# Material parameters
-E = 1e3  # Young's modulus
-nu = 0.3 # Poisson ratio
-
 # Use UFLACS to speed-up assembly
 parameters['form_compiler']['representation'] = 'uflacs'
+parameters['form_compiler']['optimize'] = True
+parameters['form_compiler']['quadrature_degree'] = 4
 
-# Create mesh
-n = 4
-height = 1.0
-length = 20.0
-mesh = RectangleMesh(0.0, 0.0, length, height, int(length*n), int(height*n), 'crossed')
 
-# Create boundary markers amd boundary measure
-boundary_parts = FacetFunction('size_t', mesh)
-right    = AutoSubDomain(lambda x: near(x[0], length))
-left     = AutoSubDomain(lambda x: near(x[0], 0.0))
-left .mark(boundary_parts, 1)
-right.mark(boundary_parts, 2)
-ds = Measure("ds", subdomain_data=boundary_parts)
+def solve_elasticity(facet_function, E, nu, dt, T_end, output_dir):
 
-# Build function space
-U = VectorFunctionSpace(mesh, "Lagrange", 2)
-P = FunctionSpace(mesh, "Lagrange", 1)
-W = MixedFunctionSpace([U, U, P])
+    # Get mesh and prepare boundary measure
+    mesh = facet_function.mesh()
+    ds = Measure("ds", subdomain_data=facet_function)
 
-# Prepare BCs
-bcs = [DirichletBC(W.sub(i), Constant((0.0, 0.0)), boundary_parts, 1)
-       for i in [0, 1]]
+    # Build function space
+    U = VectorFunctionSpace(mesh, "Lagrange", 2)
+    P = FunctionSpace(mesh, "Lagrange", 1)
+    W = MixedFunctionSpace([U, U, P])
 
-# Define constitutive law
-if nu == 0.5: # incompressible
+    # Prepare BCs
+    bcs = [DirichletBC(W.sub(i), gdim*(0.0,), facet_function, 1)
+           for i in [0, 1]]
+
+    # Define constitutive law
     mu = Constant(E/(2.0*(1.0 + nu)))
-    lmbd = Constant(0.0)
-    ilmbd= Constant(0.0)
     def stress(u, p):
         F = I + grad(u)
         J = det(F)
         B = F * F.T
-        C = F.T * F
-        E = 0.5*(C-I)
-        #W = stored energy...., then P=dW/dE
         T = -p*I + mu*(B-I) # Cauchy stress
-        P = J*T*inv(F).T # 1st Piola-Kirchhoff
-        pp = J-1.0 # incompressibility constraint or if W=f(E)+p*(J-1) then dW/dp
-        return P, pp
-else: # compressible
-    mu = Constant(E/(2.0*(1.0 + nu)))
-    lmbd = Constant(E*nu/((1.0 + nu)*(1.0 - 2.0*nu)))
-    ilmbd= 1.0/lmbd
-    def stress(u, p):
-        F = I + grad(u)
-        J = det(F)
-        B = F * F.T
-        C = F.T * F
-        E = 0.5*(C-I)
-        #W = stored energy...., then P=dW/dE
-        T = -p*I + mu*(B-I) # Cauchy stress
-        P = J*T*inv(F).T # 1st Piola-Kirchhoff stress
-        pp = ilmbd*p + (J*J-1.0)
-        return P, pp
+        if nu == 0.5:
+            # Incompressible
+            pp = J-1.0
+        else:
+            # Compressible
+            lmbd = Constant(E*nu/((1.0 + nu)*(1.0 - 2.0*nu)))
+            pp = 1.0/lmbd*p + (J*J-1.0)
+        return T, pp
 
-# Timestepping theta-method parameters
-q = Constant(0.5)
-dt = Constant(0.1)
+    # Timestepping theta-method parameters
+    q = Constant(0.5)
+    dt = Constant(dt)
 
-# Unknowns, values at previous step and test functions
-w = Function(W)
-(u, v, p) = split(w)
-w0 = Function(W)
-(u0, v0, p0) = split(w0)
-(_u, _v, _p) = TestFunctions(W)
+    # Unknowns, values at previous step and test functions
+    w = Function(W)
+    (u, v, p) = split(w)
+    w0 = Function(W)
+    (u0, v0, p0) = split(w0)
+    (_u, _v, _p) = TestFunctions(W)
 
-# Prepare variational formulation
-I = Identity(W.mesh().geometry().dim())
-T, pp = stress(u, p)
-T0, pp0 = stress(u0, p0)
-F1 = (1.0/dt)*inner(u-u0, _u)*dx - q*inner(v, _u)*dx + (1.0-q)*inner(v0, _u)*dx
-F2a = mu*inner(T, grad(_v))*dx + pp*_p*dx
-F2b = mu*inner(T0, grad(_v))*dx + pp0*_p*dx
-F2 = (1.0/dt)*inner(v-v0, _v)*dx + q*F2a + (1.0-q)*F2b
-N = FacetNormal(mesh)
-F = I + grad(u)
-n = det(F)*dot(N, inv(F).T)
-bFmag = Constant(0.0)
-bF = bFmag*det(F)*dot(inv(F).T, Constant((0.0, 1.0)))
-#bF = bFmag*Constant((0.0, 1.0))
-G = inner(Constant((0.0, 1.0)), _v)*dx
-FF = inner(bF, _v)*ds(2)
-F = F1 + F2 + FF
-J = derivative(F, w)
+    I = Identity(W.mesh().geometry().dim())
 
-# Initialize solver
-problem = NonlinearVariationalProblem(F, w, bcs=bcs, J=J)
-solver = NonlinearVariationalSolver(problem)
-solver.parameters['newton_solver']['relative_tolerance'] = 1e-6
+    # Balance of momentum
+    T, pp = stress(u, p)
+    T0, pp0 = stress(u0, p0)
+    F1 = (1.0/dt)*inner(u-u0, _u)*dx - q*inner(v, _u)*dx + (1.0-q)*inner(v0, _u)*dx
+    F2a = mu*inner(T, grad(_v))*dx + pp*_p*dx
+    F2b = mu*inner(T0, grad(_v))*dx + pp0*_p*dx
+    F2 = (1.0/dt)*inner(v-v0, _v)*dx + q*F2a + (1.0-q)*F2b
 
-# Extract solution components
-(u, v, p) = w.split()
-u.rename("u", "displacement")
-v.rename("v", "velocity")
-p.rename("p", "pressure")
+    # Traction at boundary
+    F = I + grad(u)
+    bF_magnitude = Constant(0.0)
+    bF_direction = {2: Constant((0.0, 1.0)), 3: Constant((0.0, 0.0, 1.0))}[gdim]
+    bF = det(F)*dot(inv(F).T, bF_magnitude*bF_direction)
+    FF = inner(bF, _v)*ds(2)
 
-# Create files for storing solution
-vfile = File("results/velo.xdmf")
-ufile = File("results/disp.xdmf")
-pfile = File("results/pres.xdmf")
+    # Whole system and its Jacobian
+    F = F1 + F2 + FF
+    J = derivative(F, w)
 
-# Prepare plot window
-plt = plot(u, mode="displacement", interactive=False, wireframe=True)
+    # Initialize solver
+    problem = NonlinearVariationalProblem(F, w, bcs=bcs, J=J)
+    solver = NonlinearVariationalSolver(problem)
+    solver.parameters['newton_solver']['relative_tolerance'] = 1e-6
+    solver.parameters['newton_solver']['linear_solver'] = 'mumps'
 
-# Time-stepping loop
-step = 0
-nsteps = 200
-t = 0.0
-while step < nsteps:
-    print "Step: %d, Time: %g" % (step, t)
-    t += float(dt)
-    step += 1
+    # Extract solution components
+    (u, v, p) = w.split()
+    u.rename("u", "displacement")
+    v.rename("v", "velocity")
+    p.rename("p", "pressure")
 
-    # Increase traction
-    bFmag.assign(100.0*t)
+    # Create files for storing solution
+    vfile = File("%s/velo.xdmf" % output_dir)
+    ufile = File("%s/disp.xdmf" % output_dir)
+    pfile = File("%s/pres.xdmf" % output_dir)
 
-    # Prepare to solve and solve
-    w0.assign(w)
-    solver.solve()
+    # Prepare plot window
+    plt = plot(u, mode="displacement", interactive=False, wireframe=True)
 
-    # Store solution to files and plot
-    ufile << (u, t)
-    vfile << (v, t)
-    pfile << (p, t)
-    plt.plot(u)
+    # Time-stepping loop
+    t = 0.0
+    while t <= T_end:
+        print "Time: %g" % t
+        t += float(dt)
+
+        # Increase traction
+        bF_magnitude.assign(100.0*t)
+
+        # Prepare to solve and solve
+        w0.assign(w)
+        solver.solve()
+
+        # Store solution to files and plot
+        ufile << (u, t)
+        vfile << (v, t)
+        pfile << (p, t)
+        plt.plot(u)
+
+
+def geometry_2d():
+    n = 4
+    x0 = 0.0
+    x1 = 20.0
+    y0 = 0.0
+    y1 = 1.0
+    mesh = RectangleMesh(x0, y0, x1, y1, int((x1-x0)*n), int((y1-y0)*n), 'crossed')
+    boundary_parts = FacetFunction('size_t', mesh)
+    left  = AutoSubDomain(lambda x: near(x[0], x0))
+    right = AutoSubDomain(lambda x: near(x[0], x1))
+    left .mark(boundary_parts, 1)
+    right.mark(boundary_parts, 2)
+    return boundary_parts
+
+
+def geometry_3d():
+    mesh = Mesh('lego_beam.xml')
+    gdim = mesh.geometry().dim()
+    x0 = mesh.coordinates()[:, 0].min()
+    x1 = mesh.coordinates()[:, 0].max()
+    boundary_parts = FacetFunction('size_t', mesh)
+    left  = AutoSubDomain(lambda x: near(x[0], x0))
+    right = AutoSubDomain(lambda x: near(x[0], x1))
+    left .mark(boundary_parts, 1)
+    right.mark(boundary_parts, 2)
+    return boundary_parts
+
+
+if __name__ == '__main__':
+
+    solve_elasticity(geometry_2d(), 1e3, 0.3, 0.1, 20.0)
+    solve_elasticity(geometry_2d(), 1e3, 0.5, 0.1, 20.0)
+    solve_elasticity(geometry_3d(), 1e3, 0.3, 0.1, 20.0)
