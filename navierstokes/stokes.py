65d1b581013ec1c97699dba358e841f6d7a7a911
Jan Blechta <blechta@karlin.mff.cuni.cz>
2018-09-19 19:19:58 +0200

    Add Navier-Stokes reference impl

--- navierstokes/stokes.py
+++ navierstokes/stokes.py
@@ -2,160 +2,260 @@ from dolfin import *
 import mshr
 import matplotlib.pyplot as plt
 
-# Define domain
-center = Point(0.2, 0.2)
-radius = 0.05
-L = 2.2
-W = 0.41
-geometry = mshr.Rectangle(Point(0.0, 0.0), Point(L, W)) \
-         - mshr.Circle(center, radius, 128)
 
-# Build mesh
-mesh = mshr.generate_mesh(geometry, 64)
-plot(mesh)
-plt.show()
+def build_space(N_circle, N_bulk, u_in):
+    """Prepare data for DGF benchmark. Return function
+    space, list of boundary conditions and surface measure
+    on the cylinder."""
 
-# Construct facet markers
-bndry = MeshFunction("size_t", mesh, mesh.topology().dim()-1)
-for f in facets(mesh):
-    mp = f.midpoint()
-    if near(mp[0], 0.0): # inflow
-        bndry[f] = 1
-    elif near(mp[0], L): # outflow
-        bndry[f] = 2
-    elif near(mp[1], 0.0) or near(mp[1], W): # walls
-        bndry[f] = 3
-    elif mp.distance(center) <= radius: # cylinder
-        bndry[f] = 5
+    # Define domain
+    center = Point(0.2, 0.2)
+    radius = 0.05
+    L = 2.2
+    W = 0.41
+    geometry = mshr.Rectangle(Point(0.0, 0.0), Point(L, W)) \
+             - mshr.Circle(center, radius, N_circle)
 
-# Dump facet markers to file
-with XDMFFile('facets.xdmf') as f:
-    f.write(bndry)
+    # Build mesh
+    mesh = mshr.generate_mesh(geometry, N_bulk)
 
-# Build function spaces (Taylor-Hood)
-P2 = VectorElement("P", mesh.ufl_cell(), 2)
-P1 = FiniteElement("P", mesh.ufl_cell(), 1)
-TH = MixedElement([P2, P1])
-W = FunctionSpace(mesh, TH)
+    # Construct facet markers
+    bndry = MeshFunction("size_t", mesh, mesh.topology().dim()-1)
+    for f in facets(mesh):
+        mp = f.midpoint()
+        if near(mp[0], 0.0):  # inflow
+            bndry[f] = 1
+        elif near(mp[0], L):  # outflow
+            bndry[f] = 2
+        elif near(mp[1], 0.0) or near(mp[1], W):  # walls
+            bndry[f] = 3
+        elif mp.distance(center) <= radius:  # cylinder
+            bndry[f] = 5
 
-# No-slip boundary condition for velocity on walls and cylinder - boundary id 3
-noslip = Constant((0, 0))
-bc_walls = DirichletBC(W.sub(0), noslip, bndry, 3)
-bc_cylinder = DirichletBC(W.sub(0), noslip, bndry, 5)
+    # Build function spaces (Taylor-Hood)
+    P2 = VectorElement("P", mesh.ufl_cell(), 2)
+    P1 = FiniteElement("P", mesh.ufl_cell(), 1)
+    TH = MixedElement([P2, P1])
+    W = FunctionSpace(mesh, TH)
 
-# Inflow boundary condition for velocity - boundary id 1
-v_in = Expression(("0.3 * 4.0 * x[1] * (0.41 - x[1]) / ( 0.41 * 0.41 )", "0.0"),
-                  degree=2)
-bc_in = DirichletBC(W.sub(0), v_in, bndry, 1)
+    # Prepare Dirichlet boundary conditions
+    bc_walls = DirichletBC(W.sub(0), (0, 0), bndry, 3)
+    bc_cylinder = DirichletBC(W.sub(0), (0, 0), bndry, 5)
+    bc_in = DirichletBC(W.sub(0), u_in, bndry, 1)
+    bcs = [bc_cylinder, bc_walls, bc_in]
 
-# Collect boundary conditions
-bcs = [bc_cylinder, bc_walls, bc_in]
+    # Prepare surface measure on cylinder
+    ds_circle = Measure("ds", subdomain_data=bndry, subdomain_id=5)
 
-# Facet normal, identity tensor and boundary measure
-n = FacetNormal(mesh)
-I = Identity(mesh.geometry().dim())
-ds = Measure("ds", subdomain_data=bndry)
-nu = Constant(0.001)
+    return W, bcs, ds_circle
 
 
-def stokes():
+def solve_stokes(W, nu, bcs):
+    """Solve steady Stokes and return the solution"""
 
-    # Define variational forms for Stokes
-    def a(u,v):
-        return inner(nu*grad(u), grad(v))*dx
-    def b(p,v):
-        return p*div(v)*dx
-    def L(v):
-        return inner(Constant((0.0,0.0)), v)*dx
-
-    # Solve the problem
+    # Define variational forms
     u, p = TrialFunctions(W)
     v, q = TestFunctions(W)
+    a = nu*inner(grad(u), grad(v))*dx - p*div(v)*dx - q*div(u)*dx
+    L = inner(Constant((0, 0)), v)*dx
+
+    # Solve the problem
     w = Function(W)
-    solve(a(u, v) - b(p, v) - b(q, u) == L(v), w, bcs)
-
-    # Report in,out fluxes
-    u = w.sub(0, deepcopy=False)
-    info("Inflow flux:  %e" % assemble(inner(u, n)*ds(1)))
-    info("Outflow flux: %e" % assemble(inner(u, n)*ds(2)))
-
-    # Report drag and lift
-    p = w.sub(1, deepcopy=False)
-    T = -p*I + nu*grad(u)
-    force = dot(T, n)
-    D = (force[0]/0.002)*ds(5)
-    L = (force[1]/0.002)*ds(5)
-    drag = assemble(D)
-    lift = assemble(L)
-    info("drag= %e    lift= %e" % (drag , lift))
-
-    # Report pressure difference
-    a_1 = Point(0.15, 0.2)
-    a_2 = Point(0.25, 0.2)
-    p = w.sub(1, deepcopy=False)
-    p_diff = p(a_1) - p(a_2)
-    info("p_diff = %e" % p_diff)
+    solve(a == L, w, bcs)
 
     return w
 
 
-def navier_stokes():
+def solve_navier_stokes(W, nu, bcs):
+    """Solve steady Navier-Stokes and return the solution"""
 
+    # Define variational forms
     v, q = TestFunctions(W)
     w = Function(W)
     u, p = split(w)
+    F = nu*inner(grad(u), grad(v))*dx + dot(dot(grad(u), u), v)*dx \
+        - p*div(v)*dx - q*div(u)*dx
 
-    # Define variational forms
-    T = -p*I + 2.0*nu*sym(grad(u))
-    F = inner(T, grad(v))*dx - q*div(u)*dx + inner(grad(u)*u, v)*dx
-    F += - nu*dot(dot(grad(u), v), n)*ds(2)
-
+    # Solve the problem
     solve(F == 0, w, bcs)
 
-    # Report in,out fluxes
-    u = w.sub(0, deepcopy=False)
-    info("Inflow flux:  %e" % assemble(inner(u, n)*ds(1)))
-    info("Outflow flux: %e" % assemble(inner(u, n)*ds(2)))
+    return w
+
+
+def solve_unsteady_navier_stokes(W, nu, bcs, T, dt, theta):
+    """Solver unsteady Navier-Stokes and write results
+    to file"""
+
+    # Current and old solution
+    w = Function(W)
+    u, p = split(w)
+
+    w_old = Function(W)
+    u_old, p_old = split(w_old)
+
+    # Define variational forms
+    v, q = TestFunctions(W)
+    F = ( Constant(1/dt)*dot(u - u_old, v)
+          + Constant(theta)*nu*inner(grad(u), grad(v))
+          + Constant(theta)*dot(dot(grad(u), u), v)
+          + Constant(1-theta)*nu*inner(grad(u), grad(v))
+          + Constant(1-theta)*dot(dot(grad(u_old), u_old), v)
+          - p*div(v)
+          - q*div(u)
+        )*dx
+    J = derivative(F, w)
+
+    # Create solver
+    problem = NonlinearVariationalProblem(F, w, bcs, J)
+    solver = NonlinearVariationalSolver(problem)
+    solver.parameters['newton_solver']['linear_solver'] = 'mumps'
+
+    f = XDMFFile('velocity_unteady_navier_stokes.xdmf')
+    u, p = w.split()
+
+    # Perform time-stepping
+    t = 0
+    while t < T:
+        w_old.vector()[:] = w.vector()
+        solver.solve()
+        t += dt
+        f.write(u, t)
+
+
+def save_and_plot(w, name):
+    """Saves and plots provided solution using the given
+    name"""
+
+    u, p = w.split()
+
+    # Store to file
+    with XDMFFile("results_{}/u.xdmf".format(name)) as f:
+        f.write(u)
+    with XDMFFile("results_{}/p.xdmf".format(name)) as f:
+        f.write(p)
+
+    # Plot
+    plt.figure()
+    pl = plot(u, title='velocity {}'.format(name))
+    plt.colorbar(pl)
+    plt.figure()
+    pl = plot(p, mode='warp', title='pressure {}'.format(name))
+    plt.colorbar(pl)
+
+
+def postprocess(w, nu, ds_circle):
+    """Return lift, drag and the pressure difference"""
+
+    u, p = w.split()
 
     # Report drag and lift
-    force = dot(T, n)
-    D = (force[0]/0.002)*ds(5)
-    L = (force[1]/0.002)*ds(5)
-    drag = assemble(D)
-    lift = assemble(L)
-    info("drag= %e    lift= %e" % (drag , lift))
+    n = FacetNormal(w.function_space().mesh())
+    force = -p*n + nu*dot(grad(u), n)
+    F_D = assemble(-force[0]*ds_circle)
+    F_L = assemble(-force[1]*ds_circle)
+
+    U_mean = 0.2
+    L = 0.1
+    C_D = 2/(U_mean**2*L)*F_D
+    C_L = 2/(U_mean**2*L)*F_L
 
     # Report pressure difference
     a_1 = Point(0.15, 0.2)
     a_2 = Point(0.25, 0.2)
-    p = w.sub(1, deepcopy=False)
     p_diff = p(a_1) - p(a_2)
-    info("p_diff = %e" % p_diff)
 
-    return w
+    return C_D, C_L, p_diff
+
+
+def tasks_1_2_3_4():
+    """Solve and plot alongside Stokes and Navier-Stokes"""
+
+    # Problem data
+    u_in = Expression(("4.0*U*x[1]*(0.41 - x[1])/(0.41*0.41)", "0.0"),
+                      degree=2, U=0.3)
+    nu = Constant(0.001)
+
+    # Discretization parameters
+    N_circle = 16
+    N_bulk = 64
+
+    # Prepare function space, BCs and measure on circle
+    W, bcs, ds_circle = build_space(N_circle, N_bulk, u_in)
+
+    # Solve Stokes
+    w = solve_stokes(W, nu, bcs)
+    save_and_plot(w, 'stokes')
+
+    # Solve Navier-Stokes
+    w = solve_navier_stokes(W, nu, bcs)
+    save_and_plot(w, 'navier-stokes')
+
+    # Open and hold plot windows
+    plt.show()
+
+
+def tasks_5_6():
+    """Run convergence analysis of drag and lift"""
+
+    # Problem data
+    u_in = Expression(("4.0*U*x[1]*(0.41 - x[1])/(0.41*0.41)", "0.0"),
+                      degree=2, U=0.3)
+    nu = Constant(0.001)
+
+    # Push log levelo to silence DOLFIN
+    old_level = get_log_level()
+    warning = LogLevel.WARNING if cpp.__version__ > '2017.2.0' else WARNING
+    set_log_level(warning)
+
+    fmt_header = "{:10s} | {:10s} | {:10s} | {:10s} | {:10s} | {:10s}"
+    fmt_row = "{:10d} | {:10d} | {:10d} | {:10.4f} | {:10.4f} | {:10.6f}"
+
+    # Print table header
+    print(fmt_header.format("N_bulk", "N_circle", "#dofs", "C_D", "C_L", "p_diff"))
+
+    # Solve on series of meshes
+    for N_bulk in [32, 64, 128]:
+        for N_circle in [N_bulk, 2*N_bulk, 4*N_bulk]:
+
+            # Prepare function space, BCs and measure on circle
+            W, bcs, ds_circle = build_space(N_circle, N_bulk, u_in)
+
+            # Solve Navier-Stokes
+            w = solve_navier_stokes(W, nu, bcs)
+
+            # Compute drag, lift
+            C_D, C_L, p_diff = postprocess(w, nu, ds_circle)
+            print(fmt_row.format(N_bulk, N_circle, W.dim(), C_D, C_L, p_diff))
+
+    # Pop log level
+    set_log_level(old_level)
+
+
+def task_7():
+    """Solve unsteady Navier-Stokes to resolve
+    Karman vortex street and save to file"""
+
+    # Problem data
+    u_in = Expression(("4.0*U*x[1]*(0.41 - x[1])/(0.41*0.41)", "0.0"),
+                      degree=2, U=1)
+    nu = Constant(0.001)
+    T = 8
+
+    # Discretization parameters
+    N_circle = 16
+    N_bulk = 64
+    theta = 1/2
+    dt = 0.2
+
+    # Prepare function space, BCs and measure on circle
+    W, bcs, ds_circle = build_space(N_circle, N_bulk, u_in)
+
+    # Solve unsteady Navier-Stokes
+    solve_unsteady_navier_stokes(W, nu, bcs, T, dt, theta)
 
 
 if __name__ == "__main__":
 
-    for problem in [stokes, navier_stokes]:
-        print("Running '%s'" % problem.__name__)
-
-        # Call solver
-        w = problem()
-
-        # Extract solutions
-        u, p = w.split()
-
-        # Save solution
-        u.rename("u", "velocity")
-        p.rename("p", "pressure")
-        with XDMFFile("results_%s/u.xdmf" % problem.__name__) as f:
-            f.write(u)
-        with XDMFFile("results_%s/p.xdmf" % problem.__name__) as f:
-            f.write(p)
-
-        plt.figure()
-        plot(u, title='velocity %s' % problem.__name__)
-        plt.figure()
-        plot(p, title='pressure %s' % problem.__name__)
-        plt.show()
+    tasks_1_2_3_4()
+    tasks_5_6()
+    task_7()
